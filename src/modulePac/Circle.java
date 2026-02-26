b77a517ef6c1aabef108f71a158b6c802bba1021
aryan <aryanrajput1603@gmail.com>
2026-02-27 00:35:48 +0530

    adding new files

---
+++ src/modulePac/Circle.java
@@ -0,0 +1,10 @@
+package modulePac;
+
+//import java.util.*;
+//import java.sql.*;
+public class Circle {
+	public void getArea(double d) {
+		double result = Math.PI*d*d;
+		System.out.println("Area of Circle = "+result);
+	}
+}
