55656cc684c2e88c2e057f0d47cfeafd8bb0f518
aryan <aryanrajput1603@gmail.com>
2026-03-30 22:51:43 +0530

    adding new files

--- advanceJavaProject/src/streamAPI/Employee1.java
+++ advanceJavaProject/src/streamAPI/Employee1.java
@@ -1,8 +1,12 @@
 package streamAPI;
 
+import java.nio.file.DirectoryStream.Filter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 class Employee1 {
     int id;
@@ -51,17 +55,60 @@ ArrayList<Employee1> empList = new ArrayList<>(Arrays.asList(
 
 Optional<Employee1> minSalary= empList.stream().min((a,b) ->(int)a.getSalary() - (int)b.getSalary());
 minSalary.ifPresent(System.out::println);
+System.out.println();
 
 Optional<Employee1> maxSalary= empList.stream().min((a,b) ->(int)b.getSalary() - (int)a.getSalary());
 maxSalary.ifPresent(System.out::println);
+System.out.println();
 
 Optional<Employee1> minAge= empList.stream().min((a,b) ->a.getAge() - b.getAge());
 minAge.ifPresent(System.out::println);
+System.out.println();
 
 Optional<Employee1> maxAge= empList.stream().min((a,b) ->b.getAge() - a.getAge());
 maxAge.ifPresent(System.out::println);
+System.out.println();
 
+Optional<Employee1> secondMaxSalary= empList.stream().sorted((a,b) -> Double.compare(b.getSalary(), a.getSalary())).skip(1).findFirst();
+secondMaxSalary.ifPresent(System.out::println);
+System.out.println();
 
+Optional<Employee1> secondMinSalary= empList.stream().sorted((a,b) -> Double.compare(a.getSalary(), b.getSalary())).skip(1).findFirst();
+secondMinSalary.ifPresent(System.out::println);
+System.out.println();
+
+List<Employee1> threeMaxSalary= empList.stream().sorted((a,b) -> Double.compare(b.getSalary(), a.getSalary())).limit(3).toList();
+threeMaxSalary.forEach(System.out::println);
+System.out.println();
+
+empList.stream().collect(Collectors.toMap(Employee1::getSalary, e -> e, (e1,e2) -> e1)).values().forEach(System.out::println);
+System.out.println();
+
+Employee1 result1 = empList.stream().filter(a -> a.getDepartment().equals("IT")).min(Comparator.comparing(Employee1::getSalary)).get();
+System.out.println(result1);
+System.out.println();
+
+Employee1 result2 = empList.stream().filter(a -> a.getAge() > 25).min(Comparator.comparing(Employee1::getSalary)).get();
+System.out.println(result2);
+System.out.println();
+
+empList.stream().limit(5).forEach(System.out::println);
+System.out.println();
+
+empList.stream().skip(3).forEach(System.out::println);
+System.out.println();
+
+empList.stream().sorted((a,b) -> Double.compare(a.getAge(),b.getAge())).limit(2).forEach(System.out::println);
+System.out.println();
+
+empList.stream().sorted((a,b) -> Double.compare(a.getAge(),b.getAge())).skip(2).limit(2).forEach(System.out::println);
+System.out.println();
+
+empList.stream().filter(a -> a.getDepartment().equals("IT")).sorted((a,b) -> Double.compare(b.getSalary(), a.getSalary())).limit(3).forEach(System.out::println);
+System.out.println();
+
+empList.stream().collect(Collectors.toMap(Employee1::getName, e -> e, (e1,e2) -> e1)).values().forEach(System.out::println);
+System.out.println();
 }
 
 
--- advanceJavaProject/src/streamAPI/StreamPractice.java
+++ advanceJavaProject/src/streamAPI/StreamPractice.java
@@ -11,17 +11,20 @@ public class StreamPractice {
 		list.addAll(List.of(10,15,20,25,30,11,17,16));
 		list.stream().filter(num -> num%2 == 0).forEach(System.out::println);
 		
-		ArrayList<String> listString = new ArrayList<String>();
-		listString.addAll(List.of("Amit","Ravi","Anil","Vikas","","null","Ashok","","Rahul"));
-		listString.stream().filter(str -> str.charAt(0) == 'A').forEach(str -> System.out.println(str));
 		System.out.println();
 		
-		listString.stream().filter(str -> str.length() > 4).forEach(System.out::println);
+//		ArrayList<String> listString = new ArrayList<String>();
+//		listString.addAll(List.of("Amit","Ravi","Anil","Vikas","","null","Ashok","","Rahul"));
+//		listString.stream().filter(str -> str.charAt(0) == 'A').forEach(str -> System.out.println(str));
+//		System.out.println();
+//		
+//		listString.stream().filter(str -> str.length() > 4).forEach(System.out::println);
+//		
+//		List<String> newList = listString.stream().filter(str -> !str.equals("null")).toList();
+//		System.out.println(newList);
 		
-		List<String> newList = listString.stream().filter(str -> !str.equals("null")).toList();
-		System.out.println(newList);
-		
-		
+		int sum = list.stream().sorted((a,b) -> b-a).limit(3).reduce(0, (a,b) -> a+b);
+		System.out.println(sum);
 		
 	}
 
