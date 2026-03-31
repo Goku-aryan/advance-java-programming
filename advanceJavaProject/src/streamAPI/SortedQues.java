52b72f5730dd79a724975d1b4310e74bb58ffe91
aryan <aryanrajput1603@gmail.com>
2026-03-31 11:48:15 +0530

    adding new files

--- advanceJavaProject/src/streamAPI/SortedQues.java
+++ advanceJavaProject/src/streamAPI/SortedQues.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 
 public class SortedQues extends Employee{
 
@@ -66,6 +67,51 @@ public class SortedQues extends Employee{
 		
 		List<Employee> newempList = empList.stream().sorted((x,y) -> x.salary - y.salary).toList();
 		newempList.forEach(System.out::println);
-		} 
+		System.out.println();
+		
+		list.stream().sorted((a,b) -> b-a).skip(2).forEach(System.out::println);
+		System.out.println();
+		
+		Optional<Integer> result1 = list.stream().map(x -> x*3).min((a,b) -> b-a);
+		result1.ifPresent(System.out::println);
+		System.out.println();
+		
+		int result2 = list.stream().filter(x -> x>10 && x<40).reduce(0, (a,b) -> a+b);
+		System.out.println(result2);
+		System.out.println();
+		
+		Optional<Integer> result3 = list.stream().sorted((x,y) -> y-x).skip(2).min((a,b) -> b-a);
+		result3.ifPresent(System.out::println);
+		System.out.println();
+		
+		list.stream().map(x -> x*x).sorted((x,y) -> y-x).limit(4).forEach(System.out::println);;
+		System.out.println();
+		
+		int result4 = list.stream().sorted((x,y) -> x-y).limit(4).reduce(0, (a,b) -> a+b);
+		System.out.println(result4);
+		System.out.println();
+		
+		Optional<Integer> result5 = list.stream().sorted((x,y) -> x-y).skip(3).max((a,b) -> a-b);
+		result5.ifPresent(System.out::println);
+		System.out.println();
+		
+		Optional<Integer> result6 = list.stream().filter(x -> x > 20 && x%2 != 0).sorted((x,y) -> x-y).min((a,b) -> a-b);
+		result6.ifPresent(System.out::println);
+		System.out.println();
+
+		int result7 = list.stream().filter(x -> x > 30).reduce(1, (a,b) -> a*b);
+		System.out.println(result7);
+		System.out.println();
+		
+		Optional<Integer> result8 = list.stream().sorted((x,y) -> x-y).skip(2).min((a,b) -> a-b);
+		result8.ifPresent(System.out::println);
+		System.out.println();
+		
+		Optional<Integer> result9 = list.stream().filter(x -> x%2 != 0).map(x -> x*x).sorted((x,y) -> y-x).max((a,b) -> a-b);
+		result9.ifPresent(System.out::println);
+		System.out.println();
+		
+		
+		}
 
 }
